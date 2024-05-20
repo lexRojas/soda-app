3e24d482b40e843c51d7ee415b5abed4fc7da612
Rodrigo Rojas Chaves <66186351+lexRojas@users.noreply.github.com>
2024-05-19 21:59:36 -0600

    dddddd

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-let textoWS = 'Por favor deseo ordenar el vigoron '
-let hreftWS = "https://wa.me/50685326755?text="+ textoWS 
+let textoWS = 'Para ordenar el plato del día, por favor '
+let hreftWS = "https://wa.me/50662058851?text="+ textoWS 
 
 const Menu = () => {
   return (
     <div>
       <div class="button">
         {/* <span>Ordenar !</span>         */}
-        <a href={hreftWS}   target="_blank" rel="noreferrer"> Ordenar Plato del día !</a>
+        <a className="span-text" href={hreftWS}   target="_blank" rel="noreferrer"> Ordenar Plato del día !</a>
 
       </div>
     </div>
